d81dcf84eced3b1c2fc4cf4472e9f127bd42f2d5
Youcef SAYAD <124059401+ysayad@users.noreply.github.com>
2024-05-27 15:32:15 +0000

    fix

--- medical-dashboard-webapp/app/ui/acme-logo.tsx
+++ medical-dashboard-webapp/app/ui/acme-logo.tsx
@@ -1,5 +1,8 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { Lusitana } from 'next/font/google';
+ 
+export const lusitana = Lusitana({ weight:'400' ,subsets: ['latin'] 
+});
 
 export default function AcmeLogo() {
   return (
