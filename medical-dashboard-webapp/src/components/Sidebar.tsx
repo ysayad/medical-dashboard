21142fa353b7759f3b3abe9b7ba9dc223c9a9077
karltokoekambi <acamer.guillaume@gmail.com>
2024-05-28 17:01:06 +0200

    page actuelle mise en valeur, factorisation des links

--- medical-dashboard-webapp/src/components/Sidebar.tsx
+++ medical-dashboard-webapp/src/components/Sidebar.tsx
@@ -1,9 +1,22 @@
+"use client"
+
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import {Bell, Home, LineChart, Package, Package2, ShoppingCart, Users} from "lucide-react";
 import {Button} from "@/components/ui/button";
 import {Badge} from "@/components/ui/badge";
 
 const Sidebar = () => {
+    const pathname = usePathname();
+
+    const sidebarLinks = [
+        { href: '/hospitals', label: 'Hôpitaux' },
+        { href: '/doctors', label: 'Praticiens' },
+        { href: '/urgences', label: 'Urgences' },
+        { href: '/patients', label: 'Statistiques patients' },
+    ];
+
     return (
 <div className="flex h-full max-h-screen flex-col gap-2">
     <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
@@ -18,44 +31,18 @@ const Sidebar = () => {
     </div>
     <div className="flex-1">
         <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-            <Link
-                href="hospitals/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-                <Home className="h-4 w-4" />
-                Hôpitaux
-            </Link>
-            <Link
-                href="doctors/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-                <ShoppingCart className="h-4 w-4" />
-                Praticiens
-                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                    6
-                </Badge>
-            </Link>
-            <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-            >
-                <Package className="h-4 w-4" />
-                Products{" "}
-            </Link>
-            <Link
-                href="urgences/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-                <Users className="h-4 w-4" />
-                Urgences
-            </Link>
-            <Link
-                href="patients/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-                <LineChart className="h-4 w-4" />
-                Statistiques patients
-            </Link>
+            {sidebarLinks.map(({ href, label }) => (
+                <Link
+                    href={href}
+                    className= { pathname === href ?
+                        "flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary" :
+                        "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                    }
+                >
+                    <Home className="h-4 w-4" />
+                    {label}
+                </Link>
+            ))}
         </nav>
     </div>
 </div>
