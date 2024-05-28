504d86e8b29df6a1459815424a77120f2945c01b
karltokoekambi <acamer.guillaume@gmail.com>
2024-05-28 11:09:35 +0200

    commit initial, création de répertoires et fichiers de base pour page d'accueil

---
+++ medical-dashboard-webapp/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            {/* Contenu de la sidebar */}
+        </div>
+    );
+}
+
+export default Sidebar;
---
+++ medical-dashboard-webapp/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from '../components/Sidebar'; // Chemin vers votre composant Sidebar
+
+const Home = () => {
+    return (
+        <div className="home-page">
+            <Sidebar />
+            Test Sidebar
+        </div>
+    );
+}
+
+export default Home;
