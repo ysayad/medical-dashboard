484a74cfc1fda00731759299f77a21d10257b88d
Quentin Le_Goff <quentin.legoff44@gmail.com>
2024-06-26 16:40:44 +0200

    Ajout des scripts de création des tables

---
+++ back-end-synthea-flink/create_tables.sql
@@ -0,0 +1,61 @@
+ CREATE TABLE hospital (
+  id_organization TEXT PRIMARY KEY,
+  id_location TEXT,
+  status TEXT,
+  name TEXT,
+  telecom TEXT,
+  address_line TEXT,
+  city TEXT,
+  state TEXT,
+  postalCode TEXT,
+  country TEXT,
+  longitude NUMERIC,
+  latitude NUMERIC
+);
+
+CREATE TABLE practitioner (
+  identifier TEXT,
+  name TEXT,
+  email TEXT,
+  adress TEXT,
+  city TEXT,
+  state TEXT,
+  postalCode TEXT,
+  country TEXT,
+  gender TEXT,
+  role TEXT,
+  id_organization TEXT,
+  PRIMARY KEY (identifier),
+  FOREIGN KEY (id_organization) REFERENCES hospital(id_organization)
+);
+
+CREATE TABLE patient (
+    identifier TEXT,
+    surname TEXT,
+    familyName TEXT,
+    gender TEXT,
+    birthDate DATE,
+    telecom TEXT,
+    adress TEXT,
+    city TEXT,
+    state TEXT,
+    postalCode TEXT,
+    country TEXT, 
+    maritalStatus TEXT,
+    PRIMARY KEY(identifier)
+);
+
+CREATE TABLE visit (
+    identifier TEXT,
+    id_organization TEXT,
+    id_practitioner TEXT,
+    id_patient TEXT,
+    status TEXT,
+    startTime DATE,
+    endTime DATE,
+    type TEXT,
+    PRIMARY KEY(identifier),
+    FOREIGN KEY (id_organization) REFERENCES hospital(id_organization),
+    FOREIGN KEY (id_practitioner) REFERENCES practitioner(identifier),
+    FOREIGN KEY (id_patient) REFERENCES patient(identifier)
+);
